55490da0a15b04b6905236bc67c45886e158cb45
Sourabh Rawat <souravrawat444@gmail.com>
2026-01-22 00:44:45 +0530

    chore: Replace hardcoded Firebase configuration with environment variables.

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBYZybT8uMsJol36Ws2N5Rup7UAOcv2vhU",
-    authDomain: "p2p4everyone.firebaseapp.com",
-    projectId: "p2p4everyone",
-    storageBucket: "p2p4everyone.firebasestorage.app",
-    messagingSenderId: "834986476186",
-    appId: "1:834986476186:web:1807da71b1ac40c4219523",
-    measurementId: "G-Q1B6MZ3LSF"
+    apiKey: process.env.VITE_APP_API_KEY,
+    authDomain: process.env.VITE_APP_AUTH_DOMAIN,
+    projectId: process.env.VITE_APP_PROJECT_ID,
+    storageBucket: process.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_APP_MESSAGING_SENDER_ID,
+    appId: process.env.VITE_APP_APP_ID,
+    measurementId: process.env.VITE_APP_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
